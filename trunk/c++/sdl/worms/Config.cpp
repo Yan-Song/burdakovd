48489724f69cd9ce70350f6abf9b20636401684f
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2009-10-20 10:00:32 +0000

    более универсальное значение в конфиге

--- trunk/c++/sdl/worms/Config.cpp
+++ trunk/c++/sdl/worms/Config.cpp
@@ -7,5 +7,5 @@ const double Config::DiscoverTime = 0.001;
 const double Config::WormEnergyPerCell = 10.0;
 const double Config::FoodEnergyPerCell = 1.0;
 const double Config::EnergyLossPerSecond = 1;
-const double Config::DeathEnergyLevel = 10.0;
+const double Config::DeathEnergyLevel = Config::WormEnergyPerCell; // умирает, когда остаетс€ меньше одной €чейки
 const double Config::ReplicateEnergyLevel = 200.0;
