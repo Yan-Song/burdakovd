eaad91bf2f557d1b0f74e72b7fc3d283494e397a
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2009-11-05 09:35:35 +0000

    ? 40. Читаем Боресков "основные алгоритмы вычислительной геометрии" - не нашёл + Векторное пересечение (частный случай, когда векторы двумерны, считая их трехмерными с нулевой z, и возвращая z координату их произведения) + Функция проверки пересечения отрезков а также нахождение точки пересечения + 41. Рисуем отрезок со случайными концами. В точках пересечения строим маленький круг. (много отрезков? Иначе откуда же возьмутся точки пересечения?)

--- c++/sdl/primitives/primitives.cpp
+++ c++/sdl/primitives/primitives.cpp
@@ -16,21 +16,7 @@
 #include "CompoundObject.h"
 #include "MyScene.h"
 #include "Sprite.h"
-
-class PineTree : public CompoundObject
-{
-public:
-	PineTree(const Vector2D& center, const bool filled = false)
-	{
-		Center = center;		
-		
-		Add(new Triangle2D(Vector2DByCoords(15, 15), Vector2DByCoords(-15, 15), Vector2DByCoords(0, 0), Palette::Green, filled));
-
-		Add(new Triangle2D(Vector2DByCoords(10, 0), Vector2DByCoords(-10, 0), Vector2DByCoords(0, -10), Palette::Green, filled));
-	
-		Add(new Triangle2D(Vector2DByCoords(5, -10), Vector2DByCoords(-5, -10), Vector2DByCoords(0, -15), Palette::Green, filled));
-	}
-};
+#include "Geometry2D.h"
 
 PrimitivesApplication::PrimitivesApplication() : scene(ScreenWidth, ScreenHeight)
 {
@@ -38,23 +24,26 @@ PrimitivesApplication::PrimitivesApplication() : scene(ScreenWidth, ScreenHeight
 	SDL_WM_SetCaption("Demo", NULL);
 	lasttime = time(NULL);
 		
-	scene.Center = ScreenPointByCoords(ScreenWidth / 2, ScreenHeight / 2);
+	scene.Center = Vector00;
 	
-	int TreeR = 25;
-
-	ScreenPoint dx = ScreenPointByCoords(TreeR * 2, 0);
-	ScreenPoint dy = ScreenPointByCoords(0, TreeR * 2);
-
-	for(int xx = static_cast<int>(-R / TreeR); xx < R / TreeR; ++xx)	
-		for(int yy = static_cast<int>(-R / TreeR); yy < R / TreeR; ++yy)
+	std::vector<Segment2D*> segments;
+	for(int i = 0; i < 10; ++i)
+	{
+		segments.push_back(new Segment2D(Vector2DByCoords(Rand(ScreenWidth), Rand(ScreenHeight)),
+										Vector2DByCoords(Rand(ScreenWidth), Rand(ScreenHeight)),
+										Palette::Gray));
+		scene.Add(segments[i]);
+	}
+	
+	for(int i = 0; i < segments.size(); ++i)
+		for(int j = 0; j < i; ++j)
 		{
-			Vector2D center = xx * dx + yy * dy;
-			if(center.Length() + TreeR < R)
-				scene.Add(new PineTree(center, true));
-		}
+			Segment2D* first = segments[i];
+			Segment2D* second = segments[j];
 
-	//scene.Add(new Sprite("logo.bmp"));
-	scene.Add(new Circle2D(R, Palette::Yellow));
+			if(Geometry2D::hasIntersection(first->A, first->B, second->A, second->B))
+				scene.Add(new Circle2D(Geometry2D::Intersection(first->A, first->B, second->A, second->B), 5, Palette::Yellow));
+		}
 }
 
 void PrimitivesApplication::Main()
@@ -64,10 +53,6 @@ void PrimitivesApplication::Main()
 		lasttime = time(NULL);
 		std::cout<<"Time: "<<GetTime()<<"; FPS = "<<FPS()<<", dt min/avg/max = "<<dtMin()<<"/"<<dtAvg()<<"/"<<dtMax()<<" ms."<<std::endl;
 	}
-	
-	double t = GetTime();
-	scene.Rotate(cos(t) * dt);                        // угол поворота завист от времени как phi = sin(t); dphi = cos(t) * dt
-	scene.Scale(exp(cos(t) * dt) * Vector11); // ln(size) = A + sin(t); d(ln(size)) = cos(t) * dt
 }
 
 void PrimitivesApplication::InitialRender()
