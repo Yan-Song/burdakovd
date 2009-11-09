5d3b94c50ebf43ac4312bf54d61900bb6ba72c96
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2009-11-09 17:24:58 +0000

    + оптимизации, несколько новых сфер в сцене

--- trunk/c++/sdl/RayTracing/Sphere.cpp
+++ trunk/c++/sdl/RayTracing/Sphere.cpp
@@ -3,17 +3,19 @@
 #include "Polynom.h"
 #include "SquareEquation.h"
 #include "ITracer.h"
+#include "sdlapplication/Utils.h"
 
 bool RT::Sphere::PossibleIntersection(const RT::Ray &ray) const
 {
 	const Vector3D SC = Center - ray.Start;
 	const Vector3D v = ray.Vector;
-	const double distance = (v ^ SC).Length();
+	const double qdistance = (v ^ SC).QLength();
+	const double QR = sqr(R);
 
 	// луч пересекается со сферой, если
 	// 1) расстояние от центра до него <= R (то есть прямая перескается со сферой) и
 	// 2) вектор направлен в сторону сферы либо 3) начальная точка внутри сферы
-	return (distance <= R) && ((v * SC > 0) || (Center.Distance(ray.Start) < R));
+	return (qdistance <= QR) && ((v * SC > 0) || (Center.QDistance(ray.Start) < QR));
 }
 
 class SphereTracer : public RT::ITracer
