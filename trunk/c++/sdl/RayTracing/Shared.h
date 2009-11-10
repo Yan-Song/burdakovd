60742a7e356cf7b09045a6e85fd312bdb6712fe8
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2009-11-10 07:42:46 +0000

    ~ Обновил компилятор. Теперь использую std::tr1::shared_ptr вместо boost::shared_ptr; Зависимости от boost снова нет

--- trunk/c++/sdl/RayTracing/Shared.h
+++ trunk/c++/sdl/RayTracing/Shared.h
@@ -1,11 +1,16 @@
 #ifndef SHARED_H
 #define SHARED_H
 
-#include <boost/shared_ptr.hpp>
+#include <memory>
+
+// если старый компилятор и в нём нет std::tr1::shared_ptr
+// то можно поставить boost, и:
+// #include <boost/shared_ptr.hpp>
+// using boost::shared_ptr
 
 namespace RT
 {
-	using boost::shared_ptr;
+	using std::tr1::shared_ptr;
 }
 
 #endif
