20bf2656dcac10405767191a043a8c2da382d2ac
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2009-12-09 18:41:34 +0000

    * обновил смещения для 3.3.0 (не всё проверено) * pPlayerBase теперь Pattern а не статический адрес

--- utils/Nakamar/WMemory/Patterns.cs
+++ utils/Nakamar/WMemory/Patterns.cs
@@ -20,7 +20,7 @@ namespace WoWMemoryManager
         /// <param name="mask"></param>
         public Pattern(string pattern, string mask, uint offset)
         {
-            if (pattern.Length != mask.Length * 3 - 1) throw new System.ArgumentException("несовпадение длин паттерна и маски");
+            if (pattern.Length != mask.Length * 3 - 1) throw new System.ArgumentException("Несовпадение длин паттерна и маски");
             PatternString = pattern;
             Mask = mask;
             Offset = offset;
@@ -38,13 +38,13 @@ namespace WoWMemoryManager
     static class Patterns
     {
         /// <summary>
-        /// адрес структуры g_clientConnection, works for 3.2.0
+        /// адрес структуры g_clientConnection, works for 3.3.0
         /// </summary>
         public static Pattern ClientConnection =
             new Pattern("64 8B 15 00 00 00 00 8B 34 00 8B 0D 00 00 00 00 89 81", "xxx????xx?xx????xx", 12);
         
         /// <summary>
-        /// смещение адреса ObjectManager относительно ClientConnection, works for 3.2.0
+        /// смещение адреса ObjectManager относительно ClientConnection, works for 3.3.0
         /// </summary>
         public static Pattern ObjectManagerOffset = 
             new Pattern("64 8B 15 00 00 00 00 8B 34 00 8B 0D 00 00 00 00 89 81 00 00 00 00 8B 15", "xxx????xx?xx????xx????xx", 18);
@@ -56,9 +56,14 @@ namespace WoWMemoryManager
         /// and "realmwizard", when you choose a realm
         /// </summary>        
         /// http://www.mmowned.com/forums/wow-memory-editing/260085-wow-3-2-2-info-dump-thread.html#post1684721
-        public static uint GameState = 0x010d8170;
+        public static uint GameState = 0x00B02BB8; // works for 3.3.0
 
-        public static uint PlayerBase = 0x012d4ea8; // works for 3.2.2
+        /// <summary>
+        /// Сам составил :), работает для 3.2.2 - 3.3.0
+        /// </summary>
+        public static Pattern PlayerBase =
+            new Pattern("55 8B EC 8B 45 08 81 EC 00 01 00 00 56 8B 75 0C 57 8B 7D 10 8B CE 0B CF A3 FF FF FF FF 0F 84 FF FF FF FF D9 E8",
+                "xxxxxxxxxxxxxxxxxxxxxxxxx????xx????xx", 25);
 
     }
 }
