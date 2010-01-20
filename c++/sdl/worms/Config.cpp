232f003d4644d5ab613cb12ccb97f6f97f54f682
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2010-01-20 19:14:45 +0000

    + окно графика (без рамки)

--- c++/sdl/worms/Config.cpp
+++ c++/sdl/worms/Config.cpp
@@ -9,7 +9,7 @@ const double Config::FoodEnergyPerCell = 1;
 const double Config::EnergyLossPerSecond = 1;
 const double Config::DeathEnergyLevel = 2 * Config::WormEnergyPerCell; // умирает, когда остается меньше одной ячейки
 const double Config::ReplicateEnergyLevel = 200.0;
-const int Config::CorpseRadius = 1;
+const int Config::CorpseRadius = 2;
 const int Config::InitialFoodCount = 10;
 const double Config::FoodPerSecond = 2;
 const double Config::StartEnergy = 100;
@@ -17,3 +17,7 @@ const double Config::StartEnergy = 100;
 const int Config::CellSize = 10;
 const int Config::FieldHeight = 40;
 const int Config::FieldWidth = 60;
+
+const Color Config::GraphBackground(0xd0d0f0);
+const double Config::GraphUpdateFrequency = 0.1;
+const int Config::GraphStep = 10;
