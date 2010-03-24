9e64d14fa05dbc9e72c4cf13dc04af9e0c55f6de
burdakovd <burdakovd@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2010-03-24 06:08:03 +0000

    fix

--- utils/Nakamar/Nakamar/MainForm.cs
+++ utils/Nakamar/Nakamar/MainForm.cs
@@ -203,18 +203,20 @@ namespace Nakamar
                 PlayDoNotRestartSound();
                 
             }
-            FSM = null;
+
             WoW = null;
             BotEnabled = false;
             lastDisabled = DateTime.Now;
             Log("Бот остановлен");
 
-            while (IsOneWoWRunning())
+            while (FSM.DoNotRestart && IsOneWoWRunning())
             {
                 MessageBox.Show(
                     "Произошла ошибка в работе бота, WoW не закрыт. Посмотрите, что c ним произошло, закройте WoW и затем нажмите ОК",
                     "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
+            FSM = null;
         }
 
         private List<int> WoWProcesses()
