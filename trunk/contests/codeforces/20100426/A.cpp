213a6adb2e29b260634465f2d2cffe0736393f0c
icqkill@gmail.com <icqkill@gmail.com@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2010-04-27 16:15:02 +0000

    codeforces

---
+++ trunk/contests/codeforces/20100426/A.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cassert>
+#include <iomanip>
+#include <sstream>
+ 
+#pragma comment(linker, "/STACK:16777216")
+ 
+#define mset(block,value) memset(block,value,sizeof(block))
+#define fo(i,begin,end) for(int i=begin; i<end; i++)
+#define fosz(i,s,x) for(int i=s; i<x.size(); i++)
+#define foreach(i,x) fosz(i,0,x)
+#define debug(x) cerr<<(#x)<<" = "<<(x)<<endl
+#define adebug(x,n) fo(i,0,n) cerr<<(#x)<<"["<<i<<"] = "<<x[i]<<endl
+#define vdebug(x) foreach(i,x) cerr<<(#x)<<"["<<i<<"] = "<<x[i]<<endl
+#define showv(v) foreach(i,v) cout<<v[i]<<" "; cout<<endl
+#define ALL(v) v.begin(), v.end()
+#define sqr(t) ((t)*(t))
+
+using namespace std;
+ 
+typedef long long i64;
+typedef unsigned long long u64;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<i64> v64;
+typedef vector<i64> vv64;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+ 
+const double pi=2*acos(0.0);
+const int inf=1<<25;
+
+inline int gcd(const int x, const int y)
+{
+	return y ? gcd(y, x % y) : x;
+}
+
+template<typename P, typename Q>
+inline istream& operator >>(istream& is, pair<P, Q>& p)
+{
+	return is >> p.first >> p.second;
+}
+
+template<typename P, typename Q>
+inline ostream& operator <<(ostream& os, const pair<P, Q>& p)
+{
+	return os << p.first << ' ' << p.second;
+}
+
+template<typename T>
+inline istream& operator >>(istream& is, vector<T>& v)
+{
+	for(typename vector<T>::size_type i = 0; i < v.size(); ++i)
+		is >> v[i];
+
+	return is;
+}
+
+template<typename T>
+inline ostream& operator <<(ostream& os, const vector<T>& v)
+{
+	for(typename vector<T>::size_type i = 0; i < v.size(); ++i)
+		os << (i ? " " : "") << v[i];
+
+	return os;
+}
+
+typedef string strin;
+typedef double doble;
+#define cot cout
+
+
+int main()
+{
+	int n, d;
+	cin >> n >> d;
+	
+	v64 v(n);
+	cin >> v;
+
+	i64 ans = 0;
+	for(int i = 1; i < n; ++i)
+	{
+		const i64 need = v[i-1] + 1;
+		const i64 needdelta = need > v[i] ? need - v[i] : 0;
+
+		const i64 k = (needdelta + d - 1) / d;
+		v[i] += d * k;
+		ans += k;
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
