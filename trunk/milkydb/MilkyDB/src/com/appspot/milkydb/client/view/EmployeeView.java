06378bf1364f21c43e664790018ff2f7fb220c32
icqkill@gmail.com <icqkill@gmail.com@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2010-05-05 18:14:57 +0000

    оформление

--- trunk/milkydb/MilkyDB/src/com/appspot/milkydb/client/view/EmployeeView.java
+++ trunk/milkydb/MilkyDB/src/com/appspot/milkydb/client/view/EmployeeView.java
@@ -10,7 +10,20 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class EmployeeView extends Composite implements Display {
 
-	private final Grid table = new Grid();
+	private final int Width = 3;
+	private final String Title = "Список служащих";
+	private final String[] headers = { "ФИО", "Должность", "Оклад" };
+
+	private final Grid table = new Grid(1, Width) {
+		{
+			addStyleName("listing");
+			setTitle(Title);
+			getRowFormatter().addStyleName(0, "header");
+			for (int i = 0; i < headers.length; ++i) {
+				setText(0, i, headers[i]);
+			}
+		}
+	};
 
 	public EmployeeView() {
 		initWidget(table);
@@ -23,11 +36,12 @@ public class EmployeeView extends Composite implements Display {
 
 	@Override
 	public void setData(final List<LightEmployeeDetails> employees) {
-		table.resize(employees.size(), 3);
+		table.resize(employees.size() + 1, Width);
 		for (int i = 0; i < employees.size(); ++i) {
-			table.setText(i, 0, employees.get(i).name);
-			table.setText(i, 1, employees.get(i).post);
-			table.setText(i, 2, ((Double) employees.get(i).salary).toString());
+			table.setText(i + 1, 0, employees.get(i).name);
+			table.setText(i + 1, 1, employees.get(i).post);
+			table.setText(i + 1, 2, ((Double) employees.get(i).salary)
+					.toString());
 		}
 	}
 }
