70f33da6afa44f066a81f60f5fe2b3da9de4fb62
icqkill@gmail.com <icqkill@gmail.com@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2011-05-07 18:16:41 +0000

    больше отчетов о ГБ

--- trunk/utils/Nakamar/NakamarStates/Misc/GoldReporter.cs
+++ trunk/utils/Nakamar/NakamarStates/Misc/GoldReporter.cs
@@ -26,17 +26,19 @@ namespace Plugins
             get
             {
                 AddonMessage message = Memory.GetAddonMessage();
-                if (message != null && message.lastGuildBankCheckTime > lastReportTime)
+                if (message != null && message.guildBank != null && message.guildBank.checkTime > lastReportTime)
                 {
-                    lastReportTime = message.lastGuildBankCheckTime;
+                    lastReportTime = message.guildBank.checkTime;
 
                     string target = Machine.settings["goldReports"];
                     if (target.StartsWith("http"))
                     {
                         string url = target
-                            .Replace("%SERVER%", HttpUtility.UrlEncode(message.lastGuildBankCheckServer))
-                            .Replace("%FACTION%", HttpUtility.UrlEncode(message.lastGuildBankCheckFaction))
-                            .Replace("%COPPERS%", message.guildBalance.ToString());
+                            .Replace("%SERVER%", HttpUtility.UrlEncode(message.guildBank.server))
+                            .Replace("%FACTION%", HttpUtility.UrlEncode(message.guildBank.faction))
+                            .Replace("%PLAYERNAME%", HttpUtility.UrlEncode(message.guildBank.playerName))
+                            .Replace("%COPPERS%", message.guildBank.balance.ToString())
+                            .Replace("%DELTA%", message.guildBank.delta.ToString());
 
                         Log(String.Format("GET {0}", url));
 
@@ -56,10 +58,11 @@ namespace Plugins
                     else
                     {
                         string filename = target
-                            .Replace("%SERVER%", message.lastGuildBankCheckServer)
-                            .Replace("%FACTION%", message.lastGuildBankCheckFaction);
+                            .Replace("%SERVER%", message.guildBank.server)
+                            .Replace("%FACTION%", message.guildBank.faction)
+                            .Replace("%PLAYERNAME%", message.guildBank.playerName);
 
-                        string content = message.guildBalance.ToString();
+                        string content = message.guildBank.balance.ToString() + ", " + message.guildBank.delta.ToString();
 
                         Log(String.Format("Записываю {0} в {1}", content, filename));
                         File.WriteAllText(filename, content);
