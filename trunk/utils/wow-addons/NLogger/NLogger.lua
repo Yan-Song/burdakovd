e0fc323c7710ae265157705e9750fc6da46ded1b
icqkill@gmail.com <icqkill@gmail.com@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2011-07-06 15:54:10 +0000

    logging

---
+++ trunk/utils/wow-addons/NLogger/NLogger.lua
@@ -0,0 +1,40 @@
+local private = {}
+
+local frame = CreateFrame("FRAME"); -- Need a frame to respond to events
+frame:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
+
+function frame:OnEvent(event, arg1)
+    if event == "ADDON_LOADED" and arg1 == "NLogger" then
+        -- Our saved variables are ready at this point.
+        if DefaultLog == nil then
+            DefaultLog = {}
+            print("[NLogger] Default log destination has been initialized.")
+        end
+        private.destination = DefaultLog
+    end
+end
+
+frame:SetScript("OnEvent", frame.OnEvent);
+
+
+-- constants
+local circularLogLength = 10000
+
+NLog = function(s)
+    local frameIndex = 1
+    local frameReference = _G["ChatFrame"..tostring(frameIndex)] or DEFAULT_CHAT_FRAME
+    local name, fontSize, r, g, b, a, shown, locked, docked = GetChatWindowInfo(frameIndex)
+    if (name == "") or not(docked or shown) then
+        frameReference = DEFAULT_CHAT_FRAME
+    end
+    local prefix = date("[N] [%H:%M:%S] ")
+    local message = prefix .. tostring(s)
+    local position = private.destination[0] or 1
+    private.destination[position] = message
+    private.destination[0] = position % circularLogLength + 1
+    frameReference:AddMessage(message, 1.0, 1.0, 1.0)
+end
+
+NLogDestination = function(destination)
+    private.destination = destination
+end
