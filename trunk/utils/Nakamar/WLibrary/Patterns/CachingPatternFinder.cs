f2bd61896b6d39ed0af5c3abf526f06ccc96e8b7
icqkill@gmail.com <icqkill@gmail.com@7df53c68-868c-11dd-b2bb-736fb4d8a922>
2011-07-08 21:22:59 +0000

    fix

---
+++ trunk/utils/Nakamar/WLibrary/Patterns/CachingPatternFinder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WLibrary
+{
+    public class CachingPatternFinder : PatternFinder 
+    {
+        public delegate void LogFunction(string message);
+
+        private RawMemoryManager manager;
+        private Dictionary<Pattern, uint> FindPatternCache;
+        private LogFunction Log;
+
+        public CachingPatternFinder(RawMemoryManager manager_, LogFunction logger_, Dictionary<Pattern, uint> cache)
+        {
+            this.manager = manager_;
+            this.FindPatternCache = cache;
+            this.Log = logger_;
+        }
+
+        public CachingPatternFinder(RawMemoryManager manager_, LogFunction logger_)
+            : this(manager_, logger_, new Dictionary<Pattern, uint>())
+        {
+        }
+
+        private bool CheckPattern(Pattern pattern, uint cached)
+        {
+            string[] saPattern = pattern.PatternString.Split(' ');
+            byte[] bPattern = new byte[saPattern.Length];
+
+            for (int i = 0; i < bPattern.Length; i++)
+                bPattern[i] = Convert.ToByte(saPattern[i], 0x10);
+
+            try
+            {
+                // вообще говоря если данные верны то паттерн будет находиться по адресу
+                // cached - pattern.Offset, но при ошибке WoW.FindPattern() вернёт dwStart
+                // так что делать dwStart == cached - pattern.Offset нельзя, уменьшаем на 1
+                uint answer = manager.FindPattern(cached - pattern.Offset - 1, pattern.PatternString.Length + 1, bPattern, pattern.Mask);
+                return cached == answer + pattern.Offset;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public uint FindPattern(Pattern pattern)
+        {
+            if (FindPatternCache.ContainsKey(pattern))
+            {
+                uint cached = FindPatternCache[pattern];
+                bool ok = true;
+                try
+                {
+                    if (!CheckPattern(pattern, cached))
+                        ok = false;
+                }
+                catch
+                {
+                    ok = false;
+                }
+                if (ok)
+                    return cached;
+                else
+                {
+                    Log("кэшированный оффсет FindPattern(" + pattern + ") == 0x" + cached.ToString("X8") + " устарел, ищем заново");
+                    FindPatternCache.Remove(pattern);
+                }
+            }
+
+            uint ans = manager.FindPattern(pattern.PatternString, pattern.Mask);
+            if (ans == (uint)manager.MainModule.BaseAddress)
+                throw new ApplicationException("Pattern not found");
+
+            FindPatternCache[pattern] = ans + pattern.Offset;
+            Log("найден новый оффсет FindPattern(" + pattern + ") == 0x" + FindPatternCache[pattern].ToString("X8"));
+
+            return FindPatternCache[pattern];
+        }
+
+    }
+}
---
+++ trunk/utils/Nakamar/WLibrary/Patterns/Pattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WLibrary
+{
+    /// <summary>
+    /// структура хранения паттерна, которым можно будет пользоваться в SearchPattern
+    /// </summary>
+    [Serializable]
+    public struct Pattern
+    {
+        public string PatternString;
+        public string Mask;
+        public uint Offset;
+        /// <param name="pattern">Паттерн, в виде последовательности байтов в шестнадцатеричном виде.
+        /// Байты разделяются пробелами.
+        /// Пример: "64 8B 15 00 00 00 00 8B 34 00 8B 0D 00 00 00 00 89 81"
+        /// </param>
+        /// <param name="offset">Смещение, в каком месте паттерна находится интересующая информация, индексация с 0</param>
+        /// <param name="mask"></param>
+        public Pattern(string pattern, string mask, uint offset)
+        {
+            if (pattern.Length != mask.Length * 3 - 1)
+                throw new System.ArgumentException("Несовпадение длин паттерна и маски");
+
+            PatternString = pattern;
+            Mask = mask;
+            Offset = offset;
+        }
+
+        public override string ToString()
+        {
+            return PatternString + " / " + Mask + ", " + Offset.ToString();
+        }
+    }
+}
