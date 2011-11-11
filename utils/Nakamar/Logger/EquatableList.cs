b1e27ff842b0567b1f84831f020fde9b89d454e8
icqkill <icqkill@gmail.com>
2011-11-11 16:04:38 +0000

    Refactoring

---
+++ utils/Nakamar/Logger/EquatableList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Util
+{
+    public class EquatableList<T> : List<T>, IEquatable<EquatableList<T>>
+    {
+        public EquatableList(params T[] p) : base(p)
+        {
+        }
+
+        public EquatableList()
+            : base()
+        {
+        }
+
+        #region Члены IEquatable<EquatableList<T>>
+
+        public bool Equals(EquatableList<T> other)
+        {
+            return this.SequenceEqual(other);
+        }
+
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return string.Join("|", this.Select(x => x.ToString()).ToArray()).GetHashCode();
+        }
+    }
+}
