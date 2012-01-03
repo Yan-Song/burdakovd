5f19d57753b4aa1df0349099030fe509ac77ee9e
icqkill <icqkill@gmail.com>
2012-01-03 21:36:02 +0000

    removed unused 'using' directives

--- utils/Nakamar/NakamarStates/Misc/GoldReporter.cs
+++ utils/Nakamar/NakamarStates/Misc/GoldReporter.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Net;
+using System.Web;
 using FiniteStateMachine;
 using WoWMemoryManager;
-using System.IO;
-using System.Web;
 
 namespace Plugins
 {
